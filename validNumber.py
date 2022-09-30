3d7ecfa563455b966589254aa241d4d43de87656
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2022-09-30 20:59:47 +0100

    valid number

--- validNumber.py
+++ validNumber.py
@@ -1,29 +1,41 @@
 def isNumber(self, s):
+    # create an enumerator to track when the str  is split the string
     for c,ss in enumerate(s):
         if ss in "eE":
             break 
-    else:
-        return self.is_decimal(s) or self.is_integer(s)
-    return  self.is_decimal(s[:c]) & self.is_integer(s[c+1:])
+        else:
+            return self.isDecimal(s) or self.isInteger(s)
+    return  self.isDecimal(s[:c]) & self.isInteger(s[c+1:])
 
-def is_decimal(self,st):
-    if st=="": return False
+# check if its ecimal
+def isDecimal(self,st):
+     # test edge case
+    if st=="":
+        return False
     for c,ss in enumerate(st):
-        if ss==".": break 
+        if ss==".": 
+            break 
     else: 
-        return self.is_integer(st) 
+        return self.isInteger(st) 
+
     left = st[:c]
     right = st[c+1:]
     # first operand : rule 1 and 2.3 
     # second operand: rule 2.1 and 2.2
-    return (((left in "+-") and self.is_uinteger(right)) or
-            ((self.is_integer(left) and (self.is_uinteger(right) or right==""))))
+    return (((left in "+-") and self.isUinteger(right)) or
+            ((self.isInteger(left) and (self.isUinteger(right) or right==""))))
 
-def is_uinteger(self, st):
-    if st=="": return False
+# check if its an integer without sign
+def isUinteger(self, st):
+     # test edge case
+    if st=="": 
+        return False
     return set(st).issubset("0123456789")
-def is_integer(self,st):
-    if st=="": return False
+
+def isInteger(self,st):
+    # test edge case
+    if st=="": 
+        return False
     if st[0] in "+-":
-        return self.is_uinteger(st[1:])
-    return self.is_uinteger(st)
+        return self.isUinteger(st[1:])
+    return self.isUinteger(st)
